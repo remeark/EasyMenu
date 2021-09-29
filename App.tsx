fd5835657e3ab6853256393f6ddd1c79601ae042
Marinho Kraemer <marinhokraemer97@gmail.com>
2021-09-28 21:49:40 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import AppLoading from 'expo-app-loading';
+import { ThemeProvider } from 'styled-components';
+
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins';
+
+import theme from './src/global/styles/theme';
+
+import { Dashboard } from './src/screens/Dashboard';
+
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  });
+
+  // utilizado pra segurar o app na splash enquanto carrega as fontes
+  if(!fontsLoaded){
+    return <AppLoading />
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Dashboard />
+    </ThemeProvider>
+  );
+}
+
---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { 
+    Container, 
+    Header,
+} from './styles';
+
+export function Dashboard(){
+    return(
+        <Container>
+            <Header>
+                
+
+            </Header>    
+        </Container>
+    )
+};
