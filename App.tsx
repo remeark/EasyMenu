636d2626b148659caac555ab49dc77d82e0e2b91
Marinho Kraemer <marinhokraemer97@gmail.com>
2021-10-05 21:59:15 -0300

    criado proprio hook e também a tela de login

--- App.tsx
+++ App.tsx
@@ -18,6 +18,10 @@ import theme from './src/global/styles/theme';
 
 import { AppRoutes } from './src/routes/app.routes';
 
+import { SignIn } from './src/screens/SignIn';
+
+import { AuthProvider, useAuth } from './src/hooks/auth';
+
 export default function App() {
   const [fontsLoaded] = useFonts({
     Poppins_400Regular,
@@ -32,14 +36,23 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+
       <NavigationContainer>
+
         <StatusBar 
           barStyle='light-content'
           backgroundColor="transparent"
           translucent
         />
-        <AppRoutes />
+
+        <AuthProvider>
+
+          <SignIn />
+
+        </AuthProvider>
+
       </NavigationContainer>
+
     </ThemeProvider>
   );
 }
